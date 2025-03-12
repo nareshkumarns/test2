8efaedeb2b884c349aa5aa165db9c25f0606c981
chakralanareshkumar04@gmail.com <chakralanareshkumar36@gmail.com>
2025-03-12 08:18:57 +0530

    defectfix-3456-login-error

--- test2/src/main/java/com/test2/Test2Application.java
+++ test2/src/main/java/com/test2/Test2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test2Application {
+       int x = 100;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test2Application.class, args);
