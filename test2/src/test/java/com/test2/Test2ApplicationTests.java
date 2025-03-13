43acf6441b42ad9694858443b4407e4d1a849e32
nareshkumarns <chakralanareshkumar36@gmail.com>
2025-03-13 12:43:36 +0530

    Update Test2ApplicationTests.java

--- test2/src/test/java/com/test2/Test2ApplicationTests.java
+++ test2/src/test/java/com/test2/Test2ApplicationTests.java
@@ -5,8 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test2ApplicationTests {
-   int x = 100;
-	@Test
+   public static void main(String[] args) {
+            int a = 10, b = 5;
+            double x = 12.5, y = 4.2
+            boolean flag = (a > b); // true
+	    char ch = 'B';
 	void contextLoads() {
 	}
 
