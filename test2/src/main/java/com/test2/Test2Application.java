283b2185008f3bb875b2ab6905f726367895f2c9
chakralanareshkumar04@gmail.com <chakralanareshkumar36@gmail.com>
2025-03-13 12:31:20 +0530

    some thing added

--- test2/src/main/java/com/test2/Test2Application.java
+++ test2/src/main/java/com/test2/Test2Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Test2Application.class, args);
+		SpringApplication.run(Test2Application.class, args);   
+                    boolean isSub=true;
+
 	}
 
 }
