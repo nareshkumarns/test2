81931c82324652872dab839379983dd1897f671c
chakralanareshkumar04@gmail.com <chakralanareshkumar36@gmail.com>
2025-03-17 23:16:20 +0530

    Hello

--- test2/src/main/java/com/test2/Test2Application.java
+++ test2/src/main/java/com/test2/Test2Application.java
@@ -1,28 +1,12 @@
-package com.test2;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Test2Application {
-
-	public static void main(String[] args) {
-
-		char i = 'n';
-		SpringApplication.run(Test2Application.class, args);
-
-
-    public static void main(String[] args) {
-    int a = 10, b = 5;
-    double x = 12.5, y = 4.2;
-    char ch = 'B';
-    boolean flag = (a > b); // true
-
-
-
-		SpringApplication.run(Test2Application.class, args);   
-                
-
-	}
+class A{
+ 
+class B{
 
 }
+
+//new feature 2
+
+class D{
+
+
+80% completed
